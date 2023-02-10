66f2e25148f35d2c7014b97c8604cf27d8f069f1
DavidCLuna <david.2818@outlook.com>
2023-02-10 09:16:49 -0500

    installed vuetify and config routes, now before install vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-// archivo main.js
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
+
+import { loadFonts } from './plugins/webfontloader'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import router from './router'
 
-// Vue.config.productionTip = false
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .use(router)
+  .mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import indexView from "./views/user/indexView";
+import loginView from "./views/admin/loginView";
 
 const routes = [
 
@@ -7,6 +8,11 @@ const routes = [
     path: '/',
     name: 'index',
     component: indexView
+  },
+  {
+    path: '/admin',
+    name: 'admin-login',
+    component: loginView
   },
 ]
 

f4a6b30713be63b75c26f7baf3b53f14d0df177e
DavidCLuna <david.2818@outlook.com>
2023-02-12 21:06:28 -0500

    deploy 1

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  productionSourceMap: false,
+  configureWebpack: {
+    performance: {
+      hints: 'warning',
+      maxAssetSize: 244 * 1024,
+      maxEntrypointSize: 244 * 1024
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ],
